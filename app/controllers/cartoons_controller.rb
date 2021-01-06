1e98dc8d784a9a7f5801dfb666461953d705c342
Enoch2k2 <enoch2k2@hotmail.com>
2021-01-06 11:31:58 -0600

    added crud for cartoons

--- app/controllers/cartoons_controller.rb
+++ app/controllers/cartoons_controller.rb
@@ -21,12 +21,44 @@ class CartoonsController < ApplicationController
 
   get '/cartoons/:id' do
     find_cartoon
+    redirect_if_not_exist
     erb :'cartoons/show'
+  end
+  
+  get '/cartoons/:id/edit' do
+    find_cartoon
+    redirect_if_not_exist
+    redirect_if_not_owner
+    erb :'cartoons/edit'
+  end
+
+  patch '/cartoons/:id' do
+    find_cartoon
+    if @cartoon.update(params[:cartoon])
+      redirect "/cartoons/#{@cartoon.id}"
+    else
+      redirect "/cartoons/new"
+    end
+  end
+  
+  delete '/cartoons/:id' do
+    find_cartoon
+    redirect_if_not_exist
+    @cartoon.destroy
+    redirect "/cartoons"
   end
 
   private
 
     def find_cartoon
       @cartoon = Cartoon.find_by_id(params[:id])
+    end
+
+    def redirect_if_not_owner
+      redirect "/cartoons" unless @cartoon.user == current_user
+    end
+
+    def redirect_if_not_exist
+      redirect "/cartoons" unless @cartoon
     end
 end
