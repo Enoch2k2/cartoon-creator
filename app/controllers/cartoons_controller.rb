907a8e7b60cc58f4ba4a3bfb4c147f44c2730a55
Enoch2k2 <enoch2k2@hotmail.com>
2021-01-06 11:10:35 -0600

    added cartoon creation

--- app/controllers/cartoons_controller.rb
+++ app/controllers/cartoons_controller.rb
@@ -4,4 +4,29 @@ class CartoonsController < ApplicationController
     @cartoons = Cartoon.all
     erb :'cartoons/index'
   end
+
+  get '/cartoons/new' do
+    erb :'cartoons/new'
+  end
+
+  post '/cartoons' do
+    cartoon = current_user.cartoons.build(params[:cartoon])
+
+    if cartoon.save
+      redirect '/cartoons'
+    else
+      redirect '/cartoons/new'
+    end
+  end
+
+  get '/cartoons/:id' do
+    find_cartoon
+    erb :'cartoons/show'
+  end
+
+  private
+
+    def find_cartoon
+      @cartoon = Cartoon.find_by_id(params[:id])
+    end
 end
